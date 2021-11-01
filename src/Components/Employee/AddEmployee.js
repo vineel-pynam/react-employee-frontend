5f7dabc4537c99496485d5dd9223f492701f9568
vineel-pynam <vineelpynam23@gmail.com>
2021-11-01 21:46:37 +0530

    used UseState hook

--- src/Components/Employee/AddEmployee.js
+++ src/Components/Employee/AddEmployee.js
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import { Component } from "react";
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { FiSave } from "react-icons/fi";
 import { RiArrowGoBackLine } from "react-icons/ri";
@@ -9,34 +9,19 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import "./add-employee.css";
 
-class AddEmployee extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    isLoading: false,
+const AddEmployee = (props) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const moveBack = () => {
+    props.history.push("/");
   };
 
-  onFirstNameChange = (e) => {
-    this.setState({ firstName: e.target.value });
-  };
-
-  onLastNameChange = (e) => {
-    this.setState({ lastName: e.target.value });
-  };
-
-  onEmailChange = (e) => {
-    this.setState({ email: e.target.value });
-  };
-
-  moveBack = () => {
-    this.props.history.push("/");
-  };
-
-  saveForm = async (e) => {
+  const saveForm = async (e) => {
     e.preventDefault();
-    this.setState({ isLoading: true });
-    const { firstName, lastName, email } = this.state;
+    setIsLoading(true);
     const jsonObj = {
       firstName,
       lastName,
@@ -60,12 +45,10 @@ class AddEmployee extends Component {
 
       if (status === 200) {
         toast.success("Hurrah! Employee Added");
-        this.setState({
-          firstName: "",
-          lastName: "",
-          email: "",
-          isLoading: false,
-        });
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setIsLoading(false);
       } else {
         toast.error("Unable To Add Employee");
       }
@@ -74,102 +57,93 @@ class AddEmployee extends Component {
     }
   };
 
-  render() {
-    const { firstName, lastName, email, isLoading } = this.state;
+  return (
+    <div className="form-container">
+      <h1>Add Employee</h1>
 
-    return (
-      <div className="form-container">
-        <h1>Add Employee</h1>
-
-        <form className="form">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            placeholder="Enter FirstName"
-            value={firstName}
-            onChange={this.onFirstNameChange}
-          />
-
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            placeholder="Enter LastName"
-            value={lastName}
-            onChange={this.onLastNameChange}
-          />
-
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            placeholder="Enter Email Address"
-            value={email}
-            onChange={this.onEmailChange}
-          />
-
-          <div className="button-group">
-            <Button
-              variant="contained"
-              sx={{
-                marginRight: "10px",
-                minWidth: "100px",
-              }}
-              size="medium"
-              onClick={this.saveForm}
-            >
-              {isLoading ? (
-                <div
-                  style={{
-                    marginRight: "5px",
-                    marginLeft: "0px",
-                  }}
-                >
-                  <Loader
-                    type="TailSpin"
-                    color="white"
-                    height={12}
-                    width={12}
-                  />
-                </div>
-              ) : (
-                <FiSave style={{ marginRight: "5px" }} />
-              )}
-              {isLoading ? <>saving...</> : <>save</>}
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                minWidth: "100px",
-              }}
-              size="medium"
-              color="error"
-              onClick={this.moveBack}
-            >
-              <RiArrowGoBackLine
-                style={{ marginRight: "5px", fontSize: "13px" }}
-              />
-              Go Back
-            </Button>
-          </div>
-        </form>
-
-        <ToastContainer
-          theme="dark"
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
+      <form className="form">
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          placeholder="Enter FirstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
-      </div>
-    );
-  }
-}
+
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          placeholder="Enter LastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Enter Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="button-group">
+          <Button
+            variant="contained"
+            sx={{
+              marginRight: "10px",
+              minWidth: "100px",
+            }}
+            size="medium"
+            onClick={saveForm}
+          >
+            {isLoading ? (
+              <div
+                style={{
+                  marginRight: "5px",
+                  marginLeft: "0px",
+                }}
+              >
+                <Loader type="TailSpin" color="white" height={12} width={12} />
+              </div>
+            ) : (
+              <FiSave style={{ marginRight: "5px" }} />
+            )}
+            {isLoading ? <>saving...</> : <>save</>}
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              minWidth: "100px",
+            }}
+            size="medium"
+            color="error"
+            onClick={moveBack}
+          >
+            <RiArrowGoBackLine
+              style={{ marginRight: "5px", fontSize: "13px" }}
+            />
+            Go Back
+          </Button>
+        </div>
+      </form>
+
+      <ToastContainer
+        theme="dark"
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+};
 
 export default AddEmployee;
--- src/Components/Employee/UpdateEmployee.js
+++ src/Components/Employee/UpdateEmployee.js
@@ -1,40 +1,28 @@
 import { Button } from "@mui/material";
-import { Component } from "react";
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { FiSave } from "react-icons/fi";
 import { RiArrowGoBackLine } from "react-icons/ri";
+
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 import "./add-employee.css";
 
-class UpdateEmployee extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    isLoading: false,
+const UpdateEmployee = (props) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const moveBack = () => {
+    props.history.push("/");
   };
 
-  onFirstNameChange = (e) => {
-    this.setState({ firstName: e.target.value });
-  };
-
-  onLastNameChange = (e) => {
-    this.setState({ lastName: e.target.value });
-  };
-
-  onEmailChange = (e) => {
-    this.setState({ email: e.target.value });
-  };
-
-  moveBack = () => {
-    this.props.history.push("/");
-  };
-
-  saveForm = async (e) => {
+  const saveForm = async (e) => {
     e.preventDefault();
-    this.setState({ isLoading: true });
-    const { firstName, lastName, email } = this.state;
+    setIsLoading(true);
+
     const jsonObj = {
       firstName,
       lastName,
@@ -49,7 +37,7 @@ class UpdateEmployee extends Component {
       body: JSON.stringify(jsonObj),
     };
 
-    const { match } = this.props;
+    const { match } = props;
     const { params } = match;
     const { id } = params;
     const apiUrl = `https://vineel-employee-app.herokuapp.com/api/v1/update/employees/${id}`;
@@ -60,12 +48,10 @@ class UpdateEmployee extends Component {
 
       if (status === 200) {
         toast.success("Hurrah! Employee Updated");
-        this.setState({
-          firstName: "",
-          lastName: "",
-          email: "",
-          isLoading: false,
-        });
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setIsLoading(false);
       } else {
         toast.error("Unable To Update Employee");
       }
@@ -74,101 +60,93 @@ class UpdateEmployee extends Component {
     }
   };
 
-  render() {
-    const { firstName, lastName, email, isLoading } = this.state;
-    return (
-      <div className="form-container">
-        <h1>Update Employee</h1>
+  return (
+    <div className="form-container">
+      <h1>Add Employee</h1>
 
-        <form className="form">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            placeholder="Enter FirstName"
-            value={firstName}
-            onChange={this.onFirstNameChange}
-          />
-
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            placeholder="Enter LastName"
-            value={lastName}
-            onChange={this.onLastNameChange}
-          />
-
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            placeholder="Enter Email Address"
-            value={email}
-            onChange={this.onEmailChange}
-          />
-
-          <div className="button-group">
-            <Button
-              variant="contained"
-              sx={{
-                marginRight: "10px",
-                minWidth: "100px",
-              }}
-              size="medium"
-              onClick={this.saveForm}
-            >
-              {isLoading ? (
-                <div
-                  style={{
-                    marginRight: "5px",
-                    marginLeft: "0px",
-                  }}
-                >
-                  <Loader
-                    type="TailSpin"
-                    color="white"
-                    height={12}
-                    width={12}
-                  />
-                </div>
-              ) : (
-                <FiSave style={{ marginRight: "5px" }} />
-              )}
-              {isLoading ? <>updating...</> : <>Update</>}
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                minWidth: "100px",
-              }}
-              size="medium"
-              color="error"
-              onClick={this.moveBack}
-            >
-              <RiArrowGoBackLine
-                style={{ marginRight: "5px", fontSize: "13px" }}
-              />
-              Go Back
-            </Button>
-          </div>
-        </form>
-
-        <ToastContainer
-          theme="dark"
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
+      <form className="form">
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          placeholder="Enter FirstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
-      </div>
-    );
-  }
-}
+
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          placeholder="Enter LastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Enter Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="button-group">
+          <Button
+            variant="contained"
+            sx={{
+              marginRight: "10px",
+              minWidth: "100px",
+            }}
+            size="medium"
+            onClick={saveForm}
+          >
+            {isLoading ? (
+              <div
+                style={{
+                  marginRight: "5px",
+                  marginLeft: "0px",
+                }}
+              >
+                <Loader type="TailSpin" color="white" height={12} width={12} />
+              </div>
+            ) : (
+              <FiSave style={{ marginRight: "5px" }} />
+            )}
+            {isLoading ? <>saving...</> : <>save</>}
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              minWidth: "100px",
+            }}
+            size="medium"
+            color="error"
+            onClick={moveBack}
+          >
+            <RiArrowGoBackLine
+              style={{ marginRight: "5px", fontSize: "13px" }}
+            />
+            Go Back
+          </Button>
+        </div>
+      </form>
+
+      <ToastContainer
+        theme="dark"
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+};
 
 export default UpdateEmployee;
